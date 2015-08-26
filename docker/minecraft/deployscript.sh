6a984efb9e9abaae2e241df6324c86290d4bf732
sharkwouter <wwijsman@live.nl>
2015-08-26 17:29:15 +0200

    Combined dockerfile into deployscript and added minecraft directory

--- docker/minecraft/deployscript.sh
+++ docker/minecraft/deployscript.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 # this script is designed to build and run a gmod server
 
+# set variables
+baseimage="debian:stable"
+outputimage="sharkwouter/minecraft"
 version=$(date +"%F")
-image="sharkwouter/minecraft:${newversion}"
+serverurl="https://s3.amazonaws.com/Minecraft.Download/versions/1.8.8/minecraft_server.1.8.8.jar"
 
-docker build -t ${image} .
+# create the docker file
+cat > dockerfile << EOF
+FROM ${baseimage}
+MAINTAINER Wouter Wijsman <wwijsman@live.nl>
+RUN apt-get update && apt-get dist-upgrade
+RUN apt-get install --no-install-recommends default-jre-headless wget -y
+RUN mkdir minecraft
+RUN wget ${serverurl} -P minecraft
+RUN echo "eula=true" > eula.txt
+EOF
 
-docker run -d ${image} java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.8.jar nogui
+# update base image
+docker pull ${baseimage}
+
+# build docker image
+docker build -t ${outputimage} .
+
+# start server
+echo -e "\n Starting server:"
+docker run -d ${outputimage} java -Xmx1024M -Xms1024M -jar minecraft/minecraft_server.1.8.8.jar nogui
