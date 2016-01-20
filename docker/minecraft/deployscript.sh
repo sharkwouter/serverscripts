d551f4d93f27a13ba9f019060f8c4d6fbedcfa8b
wouterwijsman <wouter.wijsman@osudio.com>
2016-01-20 14:50:31 +0100

    Updated deployscript to new version and made it functional Now it actually does start a Minecraft server which you can connect to. The world is mounted in ~/minecraft-world.

--- docker/minecraft/deployscript.sh
+++ docker/minecraft/deployscript.sh
@@ -5,17 +5,18 @@
 baseimage="debian:stable"
 version=$(date "+%Y%m%d")
 outputimage="sharkwouter/minecraft"
-serverurl="https://s3.amazonaws.com/Minecraft.Download/versions/1.8.8/minecraft_server.1.8.8.jar"
+serverurl="https://s3.amazonaws.com/Minecraft.Download/versions/1.8.9/minecraft_server.1.8.9.jar"
 
 # create the docker file
 cat > dockerfile << EOF
 FROM ${baseimage}
 MAINTAINER Wouter Wijsman <wwijsman@live.nl>
-RUN apt-get update && apt-get dist-upgrade
+RUN apt-get update && apt-get dist-upgrade -y
 RUN apt-get install --no-install-recommends default-jre-headless wget -y
-RUN wget ${serverurl}
+RUN wget "${serverurl}" -O minecraft_server.jar
 RUN echo "eula=true" > eula.txt
-ENTRYPOINT ["java", "-Xmx1024M", "-Xms1024M", "-jar", "minecraft_server.1.8.8.jar", "nogui"]
+RUN mkdir world
+ENTRYPOINT ["java", "-Xmx1024M", "-Xms1024M", "-jar", "minecraft_server.jar", "nogui"]
 EOF
 
 # update base image
@@ -26,7 +27,8 @@ docker build -t ${outputimage} .
 
 # start server
 echo -e "\n Starting server:"
-docker run -d ${outputimage}
+mkdir -p ~/minecraft-world
+docker run -d -p=25565:25565 -v ~/minecraft-world:/world ${outputimage}
 
 # cleanup
 rm dockerfile
